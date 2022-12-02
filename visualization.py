725f70d6d8b448366ba6419d5e8f4a16a0c4e01e
billzyx <billzyx11@gmail.com>
2022-12-02 17:49:18 -0500

    add visualization

--- visualization.py
+++ visualization.py
@@ -19,7 +19,41 @@ class VisualizationWidget(QtWidgets.QWidget):
         self.setLayout(layout)
 
 
-
 class VisualizationTab(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+
+        # create main horizontal layout
+        main_layout = QtWidgets.QVBoxLayout()
+
+        # create play/stop button horizontal layout
+        play_button_layout = QtWidgets.QHBoxLayout()
+        play_button = QtWidgets.QPushButton("Play")
+        stop_button = QtWidgets.QPushButton("Stop")
+        play_button_layout.addWidget(play_button)
+        play_button_layout.addWidget(stop_button)
+        main_layout.addLayout(play_button_layout)
+
+        # create open button
+        content_layout = QtWidgets.QVBoxLayout()
+        content_layout.addStretch()
+        open_button_layout = QtWidgets.QVBoxLayout()
+        open_button_layout.setAlignment(Qt.AlignCenter)
+        open_button_layout.addStretch()
+        open_button = QtWidgets.QPushButton("Open")
+        open_button.setFixedSize(100, 50)
+        open_button_layout.addWidget(open_button)
+
+        # create placeholder text
+        placeholder_text = QtWidgets.QLabel(".cha or .txt file")
+        open_button_layout.addWidget(placeholder_text)
+        open_button_layout.addStretch()
+        content_layout.addLayout(open_button_layout)
+        content_layout.addStretch()
+
+        main_layout.addLayout(content_layout)
+
+        # set main layout
+        self.setLayout(main_layout)
+
+
