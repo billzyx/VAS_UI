ae8caabaa946252d62f19abbd41b23aa68eff072
billzyx <billzyx11@gmail.com>
2022-12-02 19:17:09 -0500

    add table in visualization

--- tab_t.py
+++ tab_t.py
@@ -1,33 +1,53 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QTabWidget, QVBoxLayout
+
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QTabWidget, QVBoxLayout, QPushButton
+
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
         # Create a QTabWidget
-        tabs = QTabWidget()
+        self.tabs = QTabWidget()
 
-        # Add tabs
-        tabs.addTab(FirstTab(), "First Tab")
-        tabs.addTab(SecondTab(), "Second Tab")
+        # Enable the close button on the tabs
+        self.tabs.setTabsClosable(True)
+        self.tabs.setMovable(True)
+
+        # Connect the tabCloseRequested signal to the close_tab slot
+        self.tabs.tabCloseRequested.connect(self.close_tab)
+        self.tabs.tabBarDoubleClicked.connect(self.add_tab)
+
+        # Create a button to add new tabs
+        self.add_button = QPushButton("Add Tab")
+
+        # Connect the clicked signal to the add_tab slot
+        self.add_button.clicked.connect(self.add_tab)
 
         # Set the layout
         layout = QVBoxLayout()
-        layout.addWidget(tabs)
+        layout.addWidget(self.add_button)
+        layout.addWidget(self.tabs)
+
         self.setLayout(layout)
 
-class FirstTab(QWidget):
-    def __init__(self):
-        super().__init__()
+        # Add a few tabs
+        for i in range(3):
+            self.tabs.addTab(QWidget(), "Tab {}".format(i))
 
-        # Add your widgets here
+    def close_tab(self, index):
+        # Get the widget for the tab
+        widget = self.tabs.widget(index)
 
-class SecondTab(QWidget):
-    def __init__(self):
-        super().__init__()
+        # Remove the widget and the tab
+        widget.deleteLater()
+        self.tabs.removeTab(index)
 
-        # Add your widgets here
+    def add_tab(self):
+        # Add a new tab with a default name
+        self.tabs.addTab(QWidget(), "New Tab")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- visualization.py
+++ visualization.py
@@ -1,22 +1,55 @@
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QPushButton, QVBoxLayout, QHBoxLayout, QTabWidget
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QPushButton, QVBoxLayout, QHBoxLayout, QTabWidget, QWidget
 
 
 class VisualizationWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        tabs = QTabWidget()
+        # Create a QTabWidget
+        self.tabs = QTabWidget()
 
-        # Add tabs
-        tabs.addTab(VisualizationTab(), "First Tab")
-        tabs.addTab(VisualizationTab(), "Second Tab")
+        # Enable the close button on the tabs
+        self.tabs.setTabsClosable(True)
+        self.tabs.setMovable(True)
+
+        # Connect the tabCloseRequested signal to the close_tab slot
+        self.tabs.tabCloseRequested.connect(self.close_tab)
+        self.tabs.tabBarDoubleClicked.connect(self.add_tab)
+
+        # Create a button to add new tabs
+        self.add_button = QPushButton("Add Tab")
+
+        # Connect the clicked signal to the add_tab slot
+        self.add_button.clicked.connect(self.add_tab)
 
         # Set the layout
         layout = QVBoxLayout()
-        layout.addWidget(tabs)
+        layout.addWidget(self.add_button)
+        layout.addWidget(self.tabs)
+
         self.setLayout(layout)
+
+        self.tab_count = 0
+
+        # Add a few tabs
+        for i in range(1):
+            self.tabs_count = 0
+            self.tabs.addTab(VisualizationTab(), "Tab {}".format(i))
+
+    def close_tab(self, index):
+        # Get the widget for the tab
+        widget = self.tabs.widget(index)
+
+        # Remove the widget and the tab
+        widget.deleteLater()
+        self.tabs.removeTab(index)
+
+    def add_tab(self):
+        # Add a new tab with a default name
+        self.tabs_count += 1
+        self.tabs.addTab(VisualizationTab(), "Tab {}".format(self.tabs_count))
 
 
 class VisualizationTab(QtWidgets.QWidget):
@@ -24,7 +57,7 @@ class VisualizationTab(QtWidgets.QWidget):
         super().__init__()
 
         # create main horizontal layout
-        main_layout = QtWidgets.QVBoxLayout()
+        self.main_layout = QtWidgets.QVBoxLayout()
 
         # create play/stop button horizontal layout
         play_button_layout = QtWidgets.QHBoxLayout()
@@ -32,26 +65,66 @@ class VisualizationTab(QtWidgets.QWidget):
         stop_button = QtWidgets.QPushButton("Stop")
         play_button_layout.addWidget(play_button)
         play_button_layout.addWidget(stop_button)
-        main_layout.addLayout(play_button_layout)
+        self.main_layout.addLayout(play_button_layout)
 
         # create open button
-        content_layout = QtWidgets.QVBoxLayout()
-        content_layout.addStretch()
+        self.content_layout = QtWidgets.QVBoxLayout()
+        self.content_layout.addStretch()
         open_button_layout = QtWidgets.QVBoxLayout()
         open_button_layout.setAlignment(Qt.AlignCenter)
         open_button_layout.addStretch()
         open_button = QtWidgets.QPushButton("Open")
         open_button.setFixedSize(100, 50)
+        open_button.clicked.connect(self.open_action)
         open_button_layout.addWidget(open_button)
 
         # create placeholder text
         placeholder_text = QtWidgets.QLabel(".cha or .txt file")
         open_button_layout.addWidget(placeholder_text)
         open_button_layout.addStretch()
-        content_layout.addLayout(open_button_layout)
-        content_layout.addStretch()
+        self.content_layout.addLayout(open_button_layout)
+        self.content_layout.addStretch()
 
-        main_layout.addLayout(content_layout)
+        self.main_layout.addLayout(self.content_layout)
+
+        # set main layout
+        self.setLayout(self.main_layout)
+
+    def open_action(self):
+        while self.content_layout.count():
+            item = self.content_layout.takeAt(0)
+            if item.widget() is not None:
+                item.widget().deleteLater()
+            elif item.layout() is not None:
+                item.layout().deleteLater()
+        self.content_layout.addWidget(TableWidget())
+
+
+class TableWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Table Widget")
+
+        # create main horizontal layout
+        main_layout = QtWidgets.QHBoxLayout()
+
+        # create table
+        table = QtWidgets.QTableWidget()
+        table.setColumnCount(2)
+        table.setRowCount(2)
+        table.setHorizontalHeaderLabels(["User", "VAS"])
+        table.setVerticalHeaderLabels(["User", "VAS"])
+
+        # add play buttons to table
+        play_button1 = QtWidgets.QPushButton("Play")
+        play_button2 = QtWidgets.QPushButton("Play")
+        table.setCellWidget(0, 1, play_button1)
+        table.setCellWidget(1, 1, play_button2)
+
+        # add table to main layout and add scrollbar
+        main_layout.addWidget(table)
+        scrollbar = QtWidgets.QScrollBar()
+        main_layout.addWidget(scrollbar)
 
         # set main layout
         self.setLayout(main_layout)
